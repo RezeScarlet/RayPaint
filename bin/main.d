3903a4f23959051108c163b35dcd5261dd59877f
Aloísio M. Lingo Filho <60991374+laosioor@users.noreply.github.com>
2024-09-13 22:17:41 -0300

    coisas gamers

---
+++ bin/main.d
@@ -0,0 +1,105 @@
+bin/main.o: src/main.cpp src/../include/raylib-cpp.hpp \
+ src/../include/./AudioDevice.hpp src/../include/././raylib.hpp \
+ src/../include/././raylib-cpp-utils.hpp \
+ src/../include/././RaylibException.hpp src/../include/./././raylib.hpp \
+ src/../include/./AudioStream.hpp \
+ src/../include/./AutomationEventList.hpp \
+ src/../include/./BoundingBox.hpp src/../include/./Camera2D.hpp \
+ src/../include/././Vector2.hpp src/../include/./././raymath.hpp \
+ src/../include/./././raylib-cpp-utils.hpp src/../include/./Camera3D.hpp \
+ src/../include/././Vector3.hpp src/../include/./Color.hpp \
+ src/../include/././Vector4.hpp src/../include/./FileData.hpp \
+ src/../include/./FileText.hpp src/../include/./Font.hpp \
+ src/../include/././TextureUnmanaged.hpp src/../include/./././Vector2.hpp \
+ src/../include/./././Material.hpp src/../include/././././raylib.hpp \
+ src/../include/././././raylib-cpp-utils.hpp \
+ src/../include/./././RaylibException.hpp src/../include/./././Image.hpp \
+ src/../include/././././RaylibException.hpp \
+ src/../include/././././Color.hpp src/../include/./Functions.hpp \
+ src/../include/./Gamepad.hpp src/../include/./Image.hpp \
+ src/../include/./Keyboard.hpp src/../include/./Material.hpp \
+ src/../include/./Matrix.hpp src/../include/././raymath.hpp \
+ src/../include/./Mesh.hpp src/../include/././BoundingBox.hpp \
+ src/../include/././Model.hpp src/../include/././MeshUnmanaged.hpp \
+ src/../include/./././BoundingBox.hpp src/../include/./././Model.hpp \
+ src/../include/./Model.hpp src/../include/./ModelAnimation.hpp \
+ src/../include/././Mesh.hpp src/../include/./Mouse.hpp \
+ src/../include/./Music.hpp src/../include/./Ray.hpp \
+ src/../include/././RayCollision.hpp src/../include/./RaylibException.hpp \
+ src/../include/./RayCollision.hpp src/../include/./Rectangle.hpp \
+ src/../include/./RenderTexture.hpp src/../include/./Shader.hpp \
+ src/../include/./Texture.hpp src/../include/./Sound.hpp \
+ src/../include/./Text.hpp src/../include/./Texture.hpp \
+ src/../include/./TextureUnmanaged.hpp src/../include/./Touch.hpp \
+ src/../include/./Vector2.hpp src/../include/./Vector3.hpp \
+ src/../include/./Vector4.hpp src/../include/./VrStereoConfig.hpp \
+ src/../include/./Wave.hpp src/../include/./Window.hpp \
+ src/../include/raygui.h src/Shapes.hpp
+src/../include/raylib-cpp.hpp:
+src/../include/./AudioDevice.hpp:
+src/../include/././raylib.hpp:
+src/../include/././raylib-cpp-utils.hpp:
+src/../include/././RaylibException.hpp:
+src/../include/./././raylib.hpp:
+src/../include/./AudioStream.hpp:
+src/../include/./AutomationEventList.hpp:
+src/../include/./BoundingBox.hpp:
+src/../include/./Camera2D.hpp:
+src/../include/././Vector2.hpp:
+src/../include/./././raymath.hpp:
+src/../include/./././raylib-cpp-utils.hpp:
+src/../include/./Camera3D.hpp:
+src/../include/././Vector3.hpp:
+src/../include/./Color.hpp:
+src/../include/././Vector4.hpp:
+src/../include/./FileData.hpp:
+src/../include/./FileText.hpp:
+src/../include/./Font.hpp:
+src/../include/././TextureUnmanaged.hpp:
+src/../include/./././Vector2.hpp:
+src/../include/./././Material.hpp:
+src/../include/././././raylib.hpp:
+src/../include/././././raylib-cpp-utils.hpp:
+src/../include/./././RaylibException.hpp:
+src/../include/./././Image.hpp:
+src/../include/././././RaylibException.hpp:
+src/../include/././././Color.hpp:
+src/../include/./Functions.hpp:
+src/../include/./Gamepad.hpp:
+src/../include/./Image.hpp:
+src/../include/./Keyboard.hpp:
+src/../include/./Material.hpp:
+src/../include/./Matrix.hpp:
+src/../include/././raymath.hpp:
+src/../include/./Mesh.hpp:
+src/../include/././BoundingBox.hpp:
+src/../include/././Model.hpp:
+src/../include/././MeshUnmanaged.hpp:
+src/../include/./././BoundingBox.hpp:
+src/../include/./././Model.hpp:
+src/../include/./Model.hpp:
+src/../include/./ModelAnimation.hpp:
+src/../include/././Mesh.hpp:
+src/../include/./Mouse.hpp:
+src/../include/./Music.hpp:
+src/../include/./Ray.hpp:
+src/../include/././RayCollision.hpp:
+src/../include/./RaylibException.hpp:
+src/../include/./RayCollision.hpp:
+src/../include/./Rectangle.hpp:
+src/../include/./RenderTexture.hpp:
+src/../include/./Shader.hpp:
+src/../include/./Texture.hpp:
+src/../include/./Sound.hpp:
+src/../include/./Text.hpp:
+src/../include/./Texture.hpp:
+src/../include/./TextureUnmanaged.hpp:
+src/../include/./Touch.hpp:
+src/../include/./Vector2.hpp:
+src/../include/./Vector3.hpp:
+src/../include/./Vector4.hpp:
+src/../include/./VrStereoConfig.hpp:
+src/../include/./Wave.hpp:
+src/../include/./Window.hpp:
+src/../include/raygui.h:
+src/Shapes.hpp:
