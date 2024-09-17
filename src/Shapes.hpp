48bb570e6f423a5da00c50ba4613daf3583ffc43
RezeScarlet <gui171204@gmail.com>
2024-09-16 22:26:38 -0300

    Ellipse and polygon working

--- src/Shapes.hpp
+++ src/Shapes.hpp
@@ -44,9 +44,38 @@ public:
 };
 class Polygon : public Shape {
 public:
+  int sides;
   void Draw();
+  Polygon(raylib::Vector2 start, raylib::Vector2 end,
+          raylib::Color lineColor = raylib::Color::Black(),
+          raylib::Color fillColor = raylib::Color::White(), int sides = 3)
+      : Shape(start, end, lineColor, fillColor), sides(sides) {}
 };
 
 inline void Line::Draw() { DrawLineV(start, end, lineColor); }
-inline void Rectangle::Draw() { DrawRectangleV(start, end, fillColor); }
+inline void Rectangle::Draw() {
+  DrawRectangleV(start, end - start, fillColor);
+  DrawRectangleLines(start.x, start.y, end.x - start.x, end.y - start.y,
+                     lineColor);
+}
+inline void Ellipse::Draw() {
+  raylib::Vector2 center = (start + end) / 2.0f;
+
+  float radiusX = fabs(end.x - start.x) / 2.0f;
+  float radiusY = fabs(end.y - start.y) / 2.0f;
+
+  DrawEllipse(center.x, center.y, radiusX, radiusY, fillColor);
+
+  DrawEllipseLines(center.x, center.y, radiusX, radiusY, lineColor);
+}
+inline void Polygon::Draw() {
+  raylib::Vector2 center = (start + end) / 2.0f;
+
+  float radius = fabs(end.x - start.x) / 2.0f; 
+
+  float rotation = atan2(end.y - start.y, end.x - start.x) * (180.0f / PI);
+
+  DrawPoly(center, sides, radius, rotation, fillColor);
+  DrawPolyLines(center, sides, radius, rotation, lineColor);
+}
 } // namespace Paint
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 #include "../include/raylib-cpp.hpp"
-#include <iostream>
 #include <memory>
 #include <raylib.h>
-#include <sys/types.h>
 #define RAYGUI_IMPLEMENTATION
 #include "../include/raygui.h"
 
 #include "Shapes.hpp"
 
-int drawMode = 0; // 0 = line, 1 = rectangle, 2 = ellipse, 3 = polygon
-int Mode = 0;     // 0 = line, 1 = rectangle, 2 = ellipse, 3 = polygon, 4 =
-                  // lineColor, 5 = fillColor
+int drawMode = 0;
+int Mode = 0; // 0 = line, 1 = rectangle, 2 = ellipse, 3 = polygon, 4 =
+              // lineColor, 5 = fillColor
 raylib::Color lineColor = raylib::Color::Black();
 raylib::Color fillColor = raylib::Color::Black();
 int main() {
@@ -21,11 +19,12 @@ int main() {
 
   float h = window.GetHeight();
   float w = window.GetWidth();
-
   std::vector<std::unique_ptr<Paint::Shape>> shapes;
+  std::unique_ptr<Paint::Shape> tempShape;
   raylib::Vector2 mouseClick(0, 0);
   raylib::Vector2 mouseRelease(0, 0);
-
+  int polySides = 3;
+  bool sidesMode = false;
   while (WindowShouldClose() == false) {
 
     BeginDrawing();
@@ -37,39 +36,28 @@ int main() {
       mouseClick.SetY(raylib::Mouse::GetY());
     }
 
-    if (IsMouseButtonDown(0)) {
-      mouseRelease.x = GetMouseX();
-      mouseRelease.y = GetMouseY();
-
-      switch (drawMode) {
-      case 0: { // não sei se isso vai ser o melhor. desse jeito qnd vc
-                // seleciona
-        // cor e depois vai desenhar ele não desenha o temp pq o mode tá
-        // como 4 PQ ACABOU DE SELECIONAR COLOR PICKER
-        DrawLineV(mouseClick, mouseRelease, lineColor);
-        break;
-      }
-      case 1: {
-        DrawRectangleV(mouseClick, mouseRelease - mouseClick, fillColor);
-        break;
-      }
-      }
-    }
-
     if (IsMouseButtonReleased(0)) {
 
       switch (drawMode) {
       case 0: {
-        Paint::Line tempLine(mouseClick, mouseRelease, lineColor);
+
         shapes.push_back(
             std::make_unique<Paint::Line>(mouseClick, mouseRelease, lineColor));
         break;
       }
       case 1: {
-        Paint::Rectangle tempRect(mouseClick, mouseRelease, lineColor,
-                                  fillColor);
         shapes.push_back(std::make_unique<Paint::Rectangle>(
-            mouseClick, mouseRelease - mouseClick, lineColor, fillColor));
+            mouseClick, mouseRelease, lineColor, fillColor));
+        break;
+      }
+      case 2: {
+        shapes.push_back(std::make_unique<Paint::Ellipse>(
+            mouseClick, mouseRelease, lineColor, fillColor));
+        break;
+      }
+      case 3: {
+        shapes.push_back(std::make_unique<Paint::Polygon>(
+            mouseClick, mouseRelease, lineColor, fillColor, polySides));
         break;
       }
       }
@@ -79,6 +67,34 @@ int main() {
       shape->Draw();
     }
 
+    if (IsMouseButtonDown(0)) {
+      mouseRelease.x = GetMouseX();
+      mouseRelease.y = GetMouseY();
+
+      switch (drawMode) {
+      case 0: {
+        Paint::Line tempLine(mouseClick, mouseRelease, lineColor);
+        DrawLineV(mouseClick, mouseRelease, lineColor);
+        tempLine.Draw();
+        break;
+      }
+      case 1: {
+        Paint::Rectangle tempRect(mouseClick, mouseRelease, lineColor,
+                                  fillColor);
+        tempRect.Draw();
+        break;
+      }
+      case 2: {
+        Paint::Ellipse tempEl(mouseClick, mouseRelease, lineColor, fillColor);
+        tempEl.Draw();
+      }
+      case 3: {
+        Paint::Polygon tempPoly(mouseClick, mouseRelease, lineColor, fillColor,
+                                polySides);
+        tempPoly.Draw();
+      }
+      }
+    }
     // InterFace
 
     raylib::Rectangle guiGroupRect(8, 8, w - 16, 34);
@@ -87,10 +103,12 @@ int main() {
 
     GuiGroupBox(guiGroupRect, "CONTROLS");
     GuiToggleGroup(
-        (Rectangle){13, 13, 120, 24},
+        (Rectangle){13, 13, 60, 24},
         "Line;Rectangle;Ellipse;Polygon;Line Color;Fill Color",
         &Mode); // serve pra qnd vc clicar em cada button, o Mode mudar
-
+    if (GuiSpinner((Rectangle){550, 13, 80, 24}, "Polygon Sides", &polySides, 3,
+                   100, sidesMode))
+      sidesMode = !sidesMode;
     if (Mode < 4) {
       drawMode = Mode;
     }
